2157449531793a59a1694b74eb015605c16576c2
meytala <meytala@users.noreply.github.com>
2018-03-05 23:14:47 +0200

    update
    updated

--- lesson_8/lesson8.py
+++ lesson_8/lesson8.py
@@ -44,3 +44,113 @@ def fibonacci(n):
        return fibonacci(n - 1) + fibonacci(n - 2)
 
 print (fibonacci (11))
+
+
+print ("question 8:")
+#For the hailstone function from homework 1, you pick a positive integer n as the start.
+# If n is even, divide it by 2.
+# If n is odd, multiply it by 3 and add 1.
+# Repeat this process until n is 1.
+# Write a recursive version of hailstone that prints out the values of the sequence and returns the number of steps.
+
+#    """Print out the hailstone sequence starting at n, and return the
+#     number of elements in the sequence.
+#
+#     10
+#     5
+#     16
+#     8
+#     4
+#     2
+#     1
+#     >>> a
+#     7
+#     """
+
+x=0
+
+def hailstone (n):
+    global x
+    x+=1
+    print (int(n))
+    if n==1:
+        return print("The number of steps is", x)
+    elif n%2==0:
+        n= n/2
+        hailstone (n)
+    else:
+        n=(n*3)+1
+        hailstone (n)
+
+
+hailstone (10)
+
+
+print ("question number 9:")
+# Question 9: I Heard You Liked Functions...
+
+# Define a function cycle that takes in three functions f1, f2, f3, as arguments.
+# cycle will return another function that should take in an integer argument n and return another function.
+# That final function should take in an argument x and cycle through applying f1, f2, and f3 to x,
+# depending on what n was.
+#
+# Here's the what the final function should do to x for a few values of n:
+#
+# n = 0, return x
+# n = 1, apply f1 to x, or return f1(x)
+# n = 2, apply f1 to x and then f2 to the result of that, or return f2(f1(x))
+# n = 3, apply f1 to x, f2 to the result of applying f1, and then f3 to the result of applying f2, or f3(f2(f1(x)))
+# n = 4, start the cycle again applying f1, then f2, then f3, then f1 again, or f1(f3(f2(f1(x))))
+# And so forth.
+
+# def add1(x):
+#     return x + 1
+# def times2(x):
+#     return x * 2
+# def add3(x):
+#     return x + 3
+#
+#
+# def cycle (add1, times2, add3):
+
+print ("additional Q")
+
+
+print ("Q1:")
+#. Write a function that takes in two numbers and recursively multiplies them together
+
+def multiply(x, y):
+    if y == 0:
+        return 0
+    elif y == 1:
+        return x
+    else:
+        return x + multiply(x, y - 1)
+
+print (multiply (4, 3))
+
+
+print ("Q2:")
+
+#2. Write a function that takes in a base and an exp and recursively computes baseexp. You are not allowed to
+#use the ** operator
+
+def base_exp (x,y):
+    if y==0:
+        return 1
+    else:
+        return x * base_exp (x, y-1)
+
+print (base_exp (4, 3))
+
+print ("Q3:")
+#3. Write a function using recursion to print numbers from n to 0
+
+def num_to_zero (n):
+    print (n)
+    if n == 1:
+        return 0
+    else:
+        return num_to_zero (n-1)
+
+print (num_to_zero (5))
